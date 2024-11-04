ef0f6a9efc18ee12bba3d4cc55bdf55e051a624a
Kamal.Karami <142025765+KamalKarami@users.noreply.github.com>
2024-11-04 19:35:43 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+import numpy as np
+import pickle
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -7,21 +9,26 @@ from absenteeism_module import absenteeism_model
 
 def Input_Output():
     data = st.file_uploader("Please Upload Your File Here", type={"csv", "txt"})
+    
     if data is not None:
         df = pd.read_csv(data)
-        st.write("داده های آپلود شده:")
+        st.write("Uploaded Data:")
         st.write(df)
+        
         model = absenteeism_model('model', 'scaler')
+        
         try:
-            model.load_and_clean_data(df) # استفاده از DataFrame آپلود شده
+            model.load_and_clean_data(df)  # استفاده از DataFrame آپلود شده
             result = ""
-            if st.button("برای پیش‌بینی کلیک کنید"):
+            
+            if st.button("Click here to Predict"):
                 result = model.predicted_outputs()
                 st.balloons()
-                st.success('خروجی به شرح زیر است:')
+                st.success('The output is as follows: ')
                 st.write(result)
+                
         except Exception as e:
-            st.error(f"خطا در پردازش داده‌ها: {e}")
+            st.error(f"Error processing data: {e}")
 
 if __name__ == '__main__':
     Input_Output()
